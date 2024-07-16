534788a63a0b2ae5b83c22b28176c339d857541c
Codechadxx <154961524+Codechadxx@users.noreply.github.com>
2024-07-16 04:38:05 +0000

    Day-3-Control structures

---
+++ Day_3.js
@@ -0,0 +1,48 @@
+// Activity 1: If-Else Statements
+// Task 1: Write a program to check if a number is positive, negative, or zero, and log the result to the console. 
+
+let n = 3
+
+// if (n == 0 ) {
+//     console.log(`The number ${n} is equal to 0`);
+// } else if (n > 0) {
+//     console.log(`The number ${n} is positive`);
+// } else{
+//     console.log(`The number ${n} is negative`);
+// }
+
+// Task 2: Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
+
+let age = 18
+
+// if (age >= 18) {
+//     console.log(`The person is eligible to vote`);
+// } else{
+//     console.log(`The person is not eligible to vote`);
+// }
+
+// Activity 2: Nested If-Else Statements
+// Task 3: Write a program to find the largest of three numbers using nested if-else statements.
+
+let a = 1
+let b = 2
+let c = 3
+
+if (a > b) {
+    if (a > c) {
+        console.log(`a is the largest number`);
+    } else{
+        console.log(`c is the largest number`);
+    }
+} else{
+    if (b > c) {
+        console.log(`b is the largest number`);
+    } else{
+        console.log(`c is the largest number`);
+    }
+} 
+
+// Activity 3: Switch Case
+// Task 4: Write a program that uses a switch case to determine the day of the week based on a number (1-7) and log the day name to the console. Task 5: Write a program that uses a switch case to assign a grade ('A', 'B', 'C', 'D', 'F') based on a score and log the grade to the console.
+
+ 
